d38895f4f34ad1f1b8c08520c6cafc881508ccc0
zinglax <dylanzingler@gmail.com>
2015-04-14 10:05:24 -0400

    TCP Client and Server (Ruby Scripts)

---
+++ tcp-client.rb
@@ -0,0 +1,20 @@
+=begin
+Author: Dylan Zingler 
+Purpose: Project 3 CMSC417
+
+Usage 
+
+=end
+
+require 'socket'      # Sockets are in standard library
+
+hostname = 'localhost'
+port = 2000
+
+s = TCPSocket.open(hostname, port)
+
+while line = s.gets   # Read lines from the socket
+  puts line.chop      # And print with platform line terminator
+end
+s.close               # Close the socket when done
+
---
+++ tcp-server.rb
@@ -0,0 +1,18 @@
+=begin
+Author: Dylan Zingler 
+Purpose: Project 3 CMSC417
+
+Usage 
+
+=end
+
+require 'socket'                # Get sockets from stdlib
+
+server = TCPServer.open(2000)   # Socket to listen on port 2000
+loop {                          # Servers run forever
+  Thread.start(server.accept) do |client|
+    client.puts(Time.now.ctime) # Send the time to the client
+	client.puts "Closing the connection. Bye!"
+    client.close                # Disconnect from the client
+  end
+}
