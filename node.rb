db5bfe7e45be38d837ca07685a56de24928b0d91
theSarthi <sarthi@terpmail.umd.edu>
2015-04-14 22:38:33 -0700

    Prints hostname

--- node.rb
+++ node.rb
@@ -8,15 +8,54 @@ Triana
 =end
 
 
+#Useful Variables
+nodes_to_addrs = File.readlines('nodes-to-addrs.txt')
+addrs_to_links = File.readlines('addrs-to-links.txt')
 
-puts "Section 1: Retrieve the neighbors of the nodes"
+
 
 =begin
-Trying to retrieve neighbors -SA
+Retrieve neighbors of the node
+(1) Get the hostname
+(2) Get links connected to hostname
+(3) Get the node_names of those links 
 =end
 
+#Run ifconfig command
 ifconfig = `ifconfig`
-#puts ifconfig
-ip = ifconfig.match(/eth0/)[0]
-puts ip
-puts "End of Section 1"
+
+#Get the line matching the inet addr (IP)
+inet_addr_all = ifconfig.match(/inet addr:(\d)+.(\d).(\d)+.(\d)+/)
+if (inet_addr_all == nil)
+	puts "ERROR: Could not retrieve inet address for the IP"
+end
+#Take the first IP from ifcofig
+inet_addr = inet_addr_all[0]
+
+#Retrieve just the IP from that string (index 10 onwards)
+ip = inet_addr[10..-1]
+
+#Get the line from ntr where node is with that IP
+node_ip_line = nodes_to_addrs.select{ |line| line =~ /#{ip}/ }
+if (node_ip_line[0] == nil)
+	puts "ERROR: Could not find the IP in the nodes_to_addrs.txt file"
+end
+#Remove the IP from string
+hostname = node_ip_line[0].split[0...1].join(' ')
+
+#Got the hostname
+puts "=Hostname: #{hostname}"
+
+puts "\n\n"
+
+
+=begin
+Section 2: TCP SERVER, packet exchange and final topology cost matrix
+TO DO - DYLAN
+=end
+
+
+=begin
+Section 3: Dikstra's implementation
+#TO DO - TRIANA
+=end
