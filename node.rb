4f9be2ff3cf47f6fed8419629fb38342ddb239dc
zinglax <dylanzingler@gmail.com>
2015-04-19 18:35:39 -0400

    got the matrices to merge properly, I think Still working on it though

--- node.rb
+++ node.rb
@@ -1,5 +1,5 @@
 =begin
-	
+  
 PROJECT 3
 Members:
 Sarthi Andley
@@ -26,15 +26,15 @@ end
 
 #Method to returns the node connected with input addr
 def ip_to_node ip
-	#Get the lines from nta where node is with that IP
-	node_ip_lines = $nodes_to_addrs.select{ |line| line =~ /#{ip}/ }
+  #Get the lines from nta where node is with that IP
+  node_ip_lines = $nodes_to_addrs.select{ |line| line =~ /#{ip}/ }
 
-	if (node_ip_lines[0] == nil)
-		puts "ERROR: Could not find the IP = #{ip} in the nodes_to_addrs.txt file"
-	end
+  if (node_ip_lines[0] == nil)
+    puts "ERROR: Could not find the IP = #{ip} in the nodes_to_addrs.txt file"
+  end
 
-	#Remove the IP and whitespace from string
-	node_ip_lines[0].split[0...1].join(' ')
+  #Remove the IP and whitespace from string
+  node_ip_lines[0].split[0...1].join(' ')
 end
 
 #Method to return the ip connected with input node returns ip of destination
@@ -79,21 +79,21 @@ def conn_ip(n_s, n_d)
 end
 
 def destination_of_addr input
-	#Get the lines from atl with input
-	destination_lines = $addrs_to_links.select{ |line| line =~ /#{input}/ }
+  #Get the lines from atl with input
+  destination_lines = $addrs_to_links.select{ |line| line =~ /#{input}/ }
 
-	if (destination_lines[0] == nil)
-		puts "ERROR: Could not find the destination link from input adderess in addrs_to_links.txt file"
-	end
-	#Remove the original address and whitespace from the line 
-	first = destination_lines[0].split[0...1].join(' ')
-	second = destination_lines[0].split[1...2].join(' ')
+  if (destination_lines[0] == nil)
+    puts "ERROR: Could not find the destination link from input adderess in addrs_to_links.txt file"
+  end
+  #Remove the original address and whitespace from the line 
+  first = destination_lines[0].split[0...1].join(' ')
+  second = destination_lines[0].split[1...2].join(' ')
 
-	if (first == input)
-		return second
-	else
-		return first
-	end
+  if (first == input)
+    return second
+  else
+    return first
+  end
 end
 
 =begin
@@ -112,7 +112,7 @@ ifconfig = `ifconfig`
 #Get the line matching the inet addr (IP)
 inet_addr_all = ifconfig.match(/inet addr:(\d)+.(\d).(\d)+.(\d)+/)
 if (inet_addr_all == nil)
-	puts "ERROR: Could not retrieve inet address for the IP"
+  puts "ERROR: Could not retrieve inet address for the IP"
 end
 #Take the first IP from ifcofig
 inet_addr = inet_addr_all[0]
@@ -132,15 +132,15 @@ neighbors = Array.new
 
 #For each address, find the destination node
 addr_lines.each{ |line| 
-					#Remove the hostname and whitespace from the line
-					line.slice! "#{hostname}"
-					addr = line.split[0...1].join(' ')
-					#puts addr
-					#find the destination link of the address
-					destination = destination_of_addr(addr)
-					#find the node with that link and push it in the array
-					neighbors.push ip_to_node(destination)
-				}
+          #Remove the hostname and whitespace from the line
+          line.slice! "#{hostname}"
+          addr = line.split[0...1].join(' ')
+          #puts addr
+          #find the destination link of the address
+          destination = destination_of_addr(addr)
+          #find the node with that link and push it in the array
+          neighbors.push ip_to_node(destination)
+        }
 #Alphaterically sorted array of neighbors
 neighbors.sort!
 
@@ -285,12 +285,12 @@ def matrix_merger(np_1, np_2)
     if np_1.neighbor_matrix == np_2.neighbor_matrix
       return "matrices are equal"
     else
-      n_mat = np_1.network_matrix.clone
+      n_mat = np_1.neighbor_matrix.clone
       
       # If the value is 0 then overwrite it with np_2's value
       n_mat.each_with_index {|v, x, y| 
         if v == 0
-          n_mat[x,y] = np_2.network_matrix[x,y]
+          n_mat[x,y] = np_2.neighbor_matrix[x,y]
         end
         
         }
@@ -395,7 +395,8 @@ begin
     neighbor_packet = net_packet_builder message
     puts neighbor_packet.to_s
     
-    matrix_merger(node_net_packet, neighbor_packet)
+    puts "THIS IS THE NEW PACKET"
+    puts matrix_merger(node_net_packet, neighbor_packet).to_s
   
   }
 
