66a32ae4d85ff8eee9c0ade66272f91c7017074c
theSarthi <sarthi@terpmail.umd.edu>
2015-04-19 19:16:17 -0700

    Merged node.rb and djisktra.rb

--- node.rb
+++ node.rb
@@ -10,6 +10,7 @@ Triana M.
 require 'socket'
 require 'matrix'
 require 'set'
+require 'json'
 
 # Globals
 server_port = 2000
@@ -447,15 +448,61 @@ puts "ENDING OF PROGRAM"
 puts host_index
 puts node_net_packet.neighbor_matrix.to_a.inspect
 puts Dir.pwd
-system("ruby dijkstra.rb #{node_net_packet.neighbor_matrix.to_a.inspect} #{host_index}")
+
+graph = node_net_packet.neighbor_matrix.to_a
+NUM_NODES = graph.length
+HOST = host_index
 
 
+def min_dist(dist, shortest)
+  min = Float::INFINITY
+  min_index = -1
+  
+  for n in 0..NUM_NODES - 1
+    if((shortest[n] == false) && (dist[n] <= min))
+      min = dist[n]
+      min_index = n
+    end
+  end
+    
+  return min_index
+end
 
 
+def print(dist, n)
+  str = "Destination  Distance\n"
+  for i in 0..NUM_NODES-1
+    str += "    #{i}\t\t#{dist[i]}\n"
+  end
+  return str
+end
 
 
+def dijkstra(graph, src)
+  dist = []
+  shortest = []
+  
+  for i in 0..(NUM_NODES-1)
+    dist[i] = Float::INFINITY
+    shortest[i] = false
+  end
 
+  dist[src] = 0
 
+  for count in 0..NUM_NODES-2
+    u = min_dist(dist, shortest);
+    shortest[u] = true;
 
+    for n in 0..NUM_NODES-1
+      if (!shortest[n] && graph[u][n] != 0 && dist[u] != Float::INFINITY && (dist[u] + graph[u][n]) < dist[n])
+        dist[n] = dist[u] + graph[u][n]
+      end
+    end
+  end
+
+  return print(dist, NUM_NODES)
+end
+
+File.open('dijkstra.csv', 'w') { |file| file.write(dijkstra(graph, 0)) }
 
 
